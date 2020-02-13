30bb44400d58e8c23174bad3cac75071c1d58eec
Vangunderfanderson <juanmajxk@gmail.com>
2020-02-13 16:37:53 +0100

    Implemented first version of fizz buzz

---
+++ Practice/Algorithms/CSharp/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using Classes;
+using System;
+using Utility;
+
+namespace FizzBuzz
+{
+    /// <summary>
+    ///     Basic algorithm that for a sequence of numbers prints: 
+    ///      - "Fizz" (number divisible by 3)
+    ///      - "Buzz" (number divisible by 5)
+    ///      - "FizzBuzz" (number divisible by both 3 and 5)
+    ///      - 
+    ///     
+    /// </summary>
+    public class FizzBuzz
+    {
+        private readonly IMessageWriter _messageWriter;
+
+        public FizzBuzz(IMessageWriter messageWriter)
+        {
+            _messageWriter = messageWriter ?? throw new ArgumentNullException("Message writer");
+        }
+
+        public void Run(int start, int end)
+        {            
+            for (int i = start; i <= end; i++)
+            {
+                var line = string.Empty;
+                if (i % 3 == 0)
+                    line += "Fizz";
+
+                if (i % 5 == 0)
+                    line += "Buzz";
+
+                if (line.IsEmpty())
+                    line += i.ToString();
+
+                _messageWriter.WriteMessage(line);
+            }
+
+        }
+
+    }
+}
---
+++ Practice/Algorithms/CSharp/FizzBuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿using Classes;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var writer = new ConsoleMessageWriter();
+            var fizzBuzz = new FizzBuzz(writer);
+            fizzBuzz.Run(1, 100);
+        }
+    }
+}
