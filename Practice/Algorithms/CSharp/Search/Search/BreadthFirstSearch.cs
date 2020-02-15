521e836461fec370601d01bf6e046b4e90554987
Vangunderfanderson <juanmajxk@gmail.com>
2020-02-15 20:56:32 +0100

    Introduced search project, the node class and implemented BFS with an example

---
+++ Practice/Algorithms/CSharp/Search/Search/BreadthFirstSearch.cs
@@ -0,0 +1,28 @@
+﻿using Classes.Result;
+using Search.Classes;
+using System.Collections.Generic;
+
+namespace Search
+{
+    public class BreadthFirstSearch<T> : ITreeSearcher<T>
+    {
+        public bool ContainsValue(Node<T> root, T value)
+            => GetNodeByValue(root, value).Succeeded;
+
+        public IResult<Node<T>> GetNodeByValue(Node<T> root, T value)
+        {
+            var nodesToVisit = new Queue<Node<T>>();
+            nodesToVisit.Enqueue(root);
+            while(nodesToVisit.Count > 0)
+            {
+                var node = nodesToVisit.Dequeue();
+                if (node.Value.Equals(value))
+                    return Result.Success(node);
+
+                if (!node.IsLeaf())
+                    node.Children.ForEach(child => nodesToVisit.Enqueue(child));
+            }
+            return Result.Fail<Node<T>>("Unable to find value in the given tree");
+        }
+    }
+}
---
+++ Practice/Algorithms/CSharp/Search/Search/ITreeSearcher.cs
@@ -0,0 +1,12 @@
+﻿using Classes.Result;
+using Search.Classes;
+
+namespace Search
+{
+    public interface ITreeSearcher<T>
+    {
+        bool ContainsValue(Node<T> root, T value);
+
+        IResult<Node<T>> GetNodeByValue(Node<T> root, T value);        
+    }
+}
